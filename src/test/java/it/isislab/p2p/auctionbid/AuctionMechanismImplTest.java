c157eb1b2388464d34fa87621ef7f193d0965cdf
ddevin96 <danieledevinco1996@gmail.com>
2020-11-24 12:12:30 +0100

    test

--- src/test/java/it/isislab/p2p/auctionbid/AuctionMechanismImplTest.java
+++ src/test/java/it/isislab/p2p/auctionbid/AuctionMechanismImplTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.TestInfo;
 
 public class AuctionMechanismImplTest {
     
-    private static AuctionMechanismImpl peer0, peer1;
+    protected AuctionMechanismImpl peer0, peer1;
 
     public AuctionMechanismImplTest() throws Exception{
         class MessageListenerImpl implements MessageListener{
@@ -32,7 +32,7 @@ public class AuctionMechanismImplTest {
         }
         
         peer0 = new AuctionMechanismImpl(0, "127.0.0.1", new MessageListenerImpl(0));
-        peer1 = new AuctionMechanismImpl(1, "172.17.0.2", new MessageListenerImpl(1));
+        peer1 = new AuctionMechanismImpl(1, "127.0.0.1", new MessageListenerImpl(1));
     }
 
     @Test
@@ -66,27 +66,26 @@ public class AuctionMechanismImplTest {
 
     @Test
     void testCreateSameBid(TestInfo testInfo) {
-        //peer0.createAuction("cane", new Date(), 100.0, "bel cane");
+        peer0.createAuction("cane", new Date(), 100.0, "bel cane");
         assertFalse(peer1.createAuction("cane", new Date(), 150.0, "altro cane"));
     }
 
     @Test
     void testListAllBids(TestInfo testInfo) {
-        //peer0.createAuction("auto", new Date(), 100.0, "bel cane");
-        //peer1.createAuction("casa", new Date(), 100.0, "bella casa");
-        //peer0.createAuction("libro", new Date(), 100.0, "bel libro");
+        peer0.createAuction("auto", new Date(), 100.0, "bel cane");
+        peer1.createAuction("casa", new Date(), 100.0, "bella casa");
+        peer0.createAuction("libro", new Date(), 100.0, "bel libro");
         ArrayList<String> arr = new ArrayList<String>();
-        arr.add("cane");
-        arr.add("canenuovo");
-        //arr.add("libro");
+        arr.add("auto");
+        arr.add("casa");
+        arr.add("libro");
         assertEquals(arr, peer0.listAuctions());
     }
 
-
-    @AfterAll
-    static void leaveAll(){
-        peer0.leaveNetwork();
-        peer1.leaveNetwork();
-    }
+    // @Test
+    // void leaveAll(){
+    //     peer0.leaveNetwork();
+    //     peer1.leaveNetwork();
+    // }
 
 }
