ee5263e154216c190552b4ac95c77aa43430eb3c
ddevin96 <danieledevinco1996@gmail.com>
2020-11-24 12:04:44 +0100

    test

--- src/test/java/it/isislab/p2p/auctionbid/AuctionMechanismImplTest.java
+++ src/test/java/it/isislab/p2p/auctionbid/AuctionMechanismImplTest.java
@@ -44,9 +44,9 @@ public class AuctionMechanismImplTest {
     void testCheckRunningBid(TestInfo testInfo) {
         try {
             Date newDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2022-12-03 12:53:23");
-            peer0.createAuction("cane", newDate, 100.0, "bel cane");
+            peer0.createAuction("canenuovo", newDate, 100.0, "bel cane nuovo");
             Thread.sleep(2000);
-            assertEquals("THIS AUCTION IS STILL RUNNING\n" + "cane", peer1.checkAuction("cane"));
+            assertEquals("THIS AUCTION IS STILL RUNNING\n" + "canenuovo", peer1.checkAuction("canenuovo"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -66,19 +66,19 @@ public class AuctionMechanismImplTest {
 
     @Test
     void testCreateSameBid(TestInfo testInfo) {
-        peer0.createAuction("cane", new Date(), 100.0, "bel cane");
+        //peer0.createAuction("cane", new Date(), 100.0, "bel cane");
         assertFalse(peer1.createAuction("cane", new Date(), 150.0, "altro cane"));
     }
 
     @Test
     void testListAllBids(TestInfo testInfo) {
-        peer0.createAuction("auto", new Date(), 100.0, "bel cane");
-        peer1.createAuction("casa", new Date(), 100.0, "bella casa");
-        peer0.createAuction("libro", new Date(), 100.0, "bel libro");
+        //peer0.createAuction("auto", new Date(), 100.0, "bel cane");
+        //peer1.createAuction("casa", new Date(), 100.0, "bella casa");
+        //peer0.createAuction("libro", new Date(), 100.0, "bel libro");
         ArrayList<String> arr = new ArrayList<String>();
-        arr.add("auto");
-        arr.add("casa");
-        arr.add("libro");
+        arr.add("cane");
+        arr.add("canenuovo");
+        //arr.add("libro");
         assertEquals(arr, peer0.listAuctions());
     }
 
